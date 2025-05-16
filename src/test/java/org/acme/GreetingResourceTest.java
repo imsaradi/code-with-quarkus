638f22c896d37b70c06273e3fe92c7987cdcc993
imsaradi <im.saradi@gmail.com>
2025-05-15 19:04:47 -0700

    Test Hello World

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -14,7 +14,7 @@ class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+             .body(is("Hello from Quarkus REST service. Running on port 8080! on Maha local"));
     }
 
 }
